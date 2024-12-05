d7ff099e1c2ceda150a7c73bdfe9be7a1d1231b6
kjs106203 <kjs106203@naver.com>
2024-12-05 14:32:31 +0900

    thymeleaf 활용

---
+++ Test/src/main/java/com/boot/test/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.boot.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test1")
+    public String test1() {
+        return "text";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "test2";
+    }
+
+    @GetMapping("/test3")
+    public String test3() {
+        return "test3";
+    }
+}
