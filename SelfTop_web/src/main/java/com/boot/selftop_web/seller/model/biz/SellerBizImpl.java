ef84b284f322b8a0696d40650858e110107d75a8
Kweon <su539@naver.com>
2024-12-18 16:43:40 +0900

    [SellerMyPage 마이페이지 조회 작업]
    sellerMyPage html 수정(thymleaf)
    sellerBiz, sellerBizImpl 추가 작업
    sellerBoardMapper query문 작성
    LoginController에 원하는 데이터 setAttribute
    SellerController에 sellerMyPage 작업

--- SelfTop_web/src/main/java/com/boot/selftop_web/seller/model/biz/SellerBizImpl.java
+++ SelfTop_web/src/main/java/com/boot/selftop_web/seller/model/biz/SellerBizImpl.java
@@ -45,6 +45,11 @@ public class SellerBizImpl implements SellerBiz {
 			return false;
 		}
 	}
+	
+	@Override
+    public SellerDto getSellerInfoByMemberNo(int member_no) {
+        return mapper.getSellerInfoByMemberNo(member_no);
+    }
 
 	@Override
 	public int insertSeller(SellerDto dto) {
