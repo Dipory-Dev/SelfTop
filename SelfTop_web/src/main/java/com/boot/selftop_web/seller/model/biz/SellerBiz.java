f038c2a50ae35f80d6f56235f1e36175cb91e679
Kweon <su539@naver.com>
2024-12-18 14:28:37 +0900

    재배치

--- SelfTop_web/src/main/java/com/boot/selftop_web/seller/model/biz/SellerBiz.java
+++ SelfTop_web/src/main/java/com/boot/selftop_web/seller/model/biz/SellerBiz.java
@@ -6,9 +6,15 @@ import com.boot.selftop_web.seller.model.dto.SellerDto;
 import com.boot.selftop_web.seller.model.dto.SellerOrderDto;
 
 public interface SellerBiz {
+<<<<<<< HEAD
 //	판매자한테 들어온 주문 정보
 	public List<SellerOrderDto> selectList(int memeberno);
 	public List<SellerOrderDto> selectSearch(String startdate, String enddate, String keyword);
+=======
+	public List<SellerDto> selectList(int memberno);
+	public List<SellerDto> selectSearch(String startdate,String enddate,String keyword);
+
+>>>>>>> d879f07 ([MainPage, sellerMyPage 수정])
 
 	public boolean idchk(String id);
 
