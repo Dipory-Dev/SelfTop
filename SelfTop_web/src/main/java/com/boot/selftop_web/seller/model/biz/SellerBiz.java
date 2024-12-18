763d23fb656540ea1f35f56433ce816d55ef137f
Kweon <su539@naver.com>
2024-12-18 16:43:40 +0900

    [SellerMyPage 마이페이지 조회 작업]
    sellerMyPage html 수정(thymleaf)
    sellerBiz, sellerBizImpl 추가 작업
    sellerBoardMapper query문 작성
    LoginController에 원하는 데이터 setAttribute
    SellerController에 sellerMyPage 작업

--- SelfTop_web/src/main/java/com/boot/selftop_web/seller/model/biz/SellerBiz.java
+++ SelfTop_web/src/main/java/com/boot/selftop_web/seller/model/biz/SellerBiz.java
@@ -2,19 +2,16 @@ package com.boot.selftop_web.seller.model.biz;
 
 import java.util.List;
 
+import com.boot.selftop_web.seller.model.dto.SellerDto;
 import com.boot.selftop_web.seller.model.dto.SellerOrderDto;
 
 public interface SellerBiz {
-<<<<<<< HEAD
 //	판매자한테 들어온 주문 정보
-	public List<SellerOrderDto> selectList(int memeberno);
+	public List<SellerOrderDto> selectList(int memberno);
 	public List<SellerOrderDto> selectSearch(String startdate, String enddate, String keyword);
-=======
-	public List<SellerDto> selectList(int memberno);
-	public List<SellerDto> selectSearch(String startdate,String enddate,String keyword);
-
->>>>>>> d879f07 ([MainPage, sellerMyPage 수정])
 
 	public boolean idchk(String id);
+	
+	SellerDto getSellerInfoByMemberNo(int member_no);
 
 }
