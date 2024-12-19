3ba0bcd09908e764d84d05495ff8a1f4cb00c7ff
ows710 <Ohwonseok@git.ajou.ac.kr>
2024-12-19 18:22:26 +0900

    12/19 [seller, customer] 충돌해결

--- SelfTop_web/src/main/java/com/boot/selftop_web/member/customer/biz/CustomerBizImpl.java
+++ SelfTop_web/src/main/java/com/boot/selftop_web/member/customer/biz/CustomerBizImpl.java
@@ -1,6 +1,7 @@
 package com.boot.selftop_web.member.customer.biz;
 
 import com.boot.selftop_web.member.customer.model.dto.CustomerDto;
+import com.boot.selftop_web.member.seller.model.dto.SellerOrderDto;
 import com.boot.selftop_web.member.customer.biz.mapper.CustomerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,14 @@ public class CustomerBizImpl implements CustomerBiz {
 		return mapper.delUser(email, pw);
 	}
 
-
+	@Override
+	public boolean verifyPW(String pw) {
+		CustomerDto res = mapper.verifyPW(pw);
+		System.out.println("biz :" + res);
+		if (res == null){
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
